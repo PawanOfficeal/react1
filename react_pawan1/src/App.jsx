4eb1a7f16b4733e6a4c1d294a7973ac0a9bd265a
Pawan Deep Naugain <pawan.officeal@gmail.com>
2024-06-06 20:56:52 -0500

    commit 1 for React Project

---
+++ react_pawan1/src/App.jsx
@@ -0,0 +1,58 @@
+/*import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App */
+
+/* ================================================================ */
+export const App = () =>{
+  return (
+   <div>
+ <div>
+      <div><img src="letsDesign3.png" alt="img1" width="20%" height="20%" /></div>
+ </div>
+
+ <h2>Name: Queen Of Tears:</h2>
+ <h3>Rating : 8.2</h3>
+ <p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, facilis! Incidunt eius minus eveniet 
+  sed voluptates aliquid, eligendi expedita consequatur maiores tempora soluta! Dolore cupiditate, dicta vel 
+  repellendus maxime, fuga,  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, inventore sit.
+  Voluptas autem nulla veritatis doloremque voluptates illum iure eaque.
+  molestias possimus dolorem quia porro nostrum dignissimos id velit saepe!
+ </p>
+ </div>
+  );
+}
+
+export default App
